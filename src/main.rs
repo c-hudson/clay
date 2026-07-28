b938b947f45627066d327886102d862d8b8d68f2
adrick <cmhudson@gmail.com>
2026-07-28 13:13:13 -0500

    Bump version to v1.4.3

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ pub mod testserver;
 pub mod testharness;
 
 // Version information
-const VERSION: &str = "1.4.2";
+const VERSION: &str = "1.4.3";
 const BUILD_HASH: &str = env!("BUILD_HASH");
 const BUILD_DATE: &str = env!("BUILD_DATE");
 
