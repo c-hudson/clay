f2f52a4ea6beee27cdcc144b869a58239e27b725
adrick <cmhudson@gmail.com>
2026-08-09 00:13:32 -0500

    Bump version to v1.5.17

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ pub mod testserver;
 pub mod testharness;
 
 // Version information
-pub(crate) const VERSION: &str = "1.5.16";
+pub(crate) const VERSION: &str = "1.5.17";
 const BUILD_HASH: &str = env!("BUILD_HASH");
 const BUILD_DATE: &str = env!("BUILD_DATE");
 
